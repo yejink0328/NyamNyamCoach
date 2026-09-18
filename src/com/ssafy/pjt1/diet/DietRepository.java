f13c1d47b0cf1457516827be5c7e7631234e428a
yejink0328 <yejink0328@gmail.com>
2026-09-18 15:30:57 +0900

    refactor: separate diet domain and repository responsibilities

--- src/com/ssafy/pjt1/diet/DietRepository.java
+++ src/com/ssafy/pjt1/diet/DietRepository.java
@@ -1,5 +1,16 @@
 package com.ssafy.pjt1.diet;
 
-public class DietRepository {
+import java.util.ArrayList;
+import java.util.List;
 
+public class DietRepository {
+	private final List<DietDomain> diets = new ArrayList<>();
+	
+	private int nextId = 1;
+
+	public void save(DietDomain diet) {
+		diet.setDietId(nextId++);
+		diets.add(diet);
+	}
 }
+
